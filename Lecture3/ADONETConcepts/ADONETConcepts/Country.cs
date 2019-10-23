64d85fdb82604eedd260690b785d78ecfc2f887a
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-10-23 21:06:52 +0500

    Lecture 3
    Introduction to ADO.NET.

---
+++ Lecture3/ADONETConcepts/ADONETConcepts/Country.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADONETConcepts
+{
+    public partial class frmCountry : Form
+    {
+        public frmCountry()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCountry_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'pNYASPBatch19DataSet.Country' table. You can move, or remove it, as needed.
+            this.countryTableAdapter.Fill(this.pNYASPBatch19DataSet.Country);
+
+        }
+    }
+}
---
+++ Lecture3/Basic CURD Quries.sql
@@ -0,0 +1,4 @@
+--INSERT INTO Country ([Name]) VALUES ('Afghanistan');
+SELECT * FROM Country;
+--UPDATE Country SET [Name] = 'China' WHERE ID = 3;
+--DELETE FROM Country WHERE ID = 2;
