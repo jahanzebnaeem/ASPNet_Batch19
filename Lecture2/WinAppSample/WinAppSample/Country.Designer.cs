44263d67334549361d0662dc0925d3bf0947743d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-10-23 19:10:07 +0500

    Lecture 2
    The code for introduction to windows forms.

---
+++ Lecture2/WinAppSample/WinAppSample/Country.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WinAppSample
+{
+    partial class frmCountry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name: ";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(12, 64);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(59, 6);
+            this.txtName.MaxLength = 50;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(173, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(157, 36);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(59, 107);
+            this.txtNumber.MaxLength = 50;
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(173, 20);
+            this.txtNumber.TabIndex = 2;
+            // 
+            // frmCountry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 145);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCountry";
+            this.Text = "Country";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtNumber;
+    }
+}
+
---
+++ Lecture2/WinAppSample/WinAppSample/Country.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppSample
+{
+    public partial class frmCountry : Form
+    {
+        public frmCountry()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Iteration 1
+            // lblMessage.Text = txtName.Text + ", hello and welcome to batch 19";
+
+            // Iteration 2
+            // Add two textboxes and add there values.
+            // lblMessage.Text = Convert.ToString(Convert.ToInt32(txtName.Text) + Convert.ToInt32(lblNumber.Text));
+
+            // Iteration 3
+            // Work it with method
+            // calculateValue();
+
+            // Iteration 4
+            // Work it with function
+            lblMessage.Text = calculateValue(Convert.ToInt32(txtName.Text), Convert.ToInt32(txtNumber.Text));
+        }
+
+        /// <summary>
+        /// Requires to generate a method
+        /// <para>No Parameters</para>
+        /// <return>Nothing</return>
+        /// </summary>
+        private void calculateValue()
+        {
+            lblMessage.Text = Convert.ToString(Convert.ToInt32(txtName.Text) + Convert.ToInt32(txtNumber.Text));
+        }
+
+        /// <summary>
+        /// Adds the values of two controls
+        /// </summary>
+        /// <param name="a">Operand 1 in integer</param>
+        /// <param name="b">Operand 2 in integer</param>
+        /// <return>String value</return>
+        /// <returns></returns>
+        private string calculateValue(int a, int b)
+        {
+            // Code logic
+            return Convert.ToString(a + b);
+        }
+    }
+}
