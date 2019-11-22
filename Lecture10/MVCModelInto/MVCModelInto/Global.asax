8b8a8b7d18dd8d1b9414e3db4ca1feacf04c5e5d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-11-22 15:48:50 +0500

    MVC Model - Intro
    This is the introduction to MVC Models.

---
+++ Lecture10/MVCModelInto/MVCModelInto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCModelInto.MvcApplication" Language="C#" %>
---
+++ Lecture10/MVCModelInto/MVCModelInto/Models/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCModelInto.Models
+{
+    public class City
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [MinLength(4)]
+        public String Name { get; set; }
+        public int CountryID { get; set; }
+        public virtual Country Country { get; set; }
+    }
+}
