7bf0235046254eb99c9348ce386de376dea1e936
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-11-26 21:15:49 +0500

    Lecture 11
    The power of Visual Studio and Entity Framwork.

---
+++ Lecture11/MVCControllerIntro/MVCControllerIntro/Migrations/201911261602096_countrymodel-update-v1.cs
@@ -0,0 +1,16 @@
+namespace MVCControllerIntro.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class countrymodelupdatev1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
