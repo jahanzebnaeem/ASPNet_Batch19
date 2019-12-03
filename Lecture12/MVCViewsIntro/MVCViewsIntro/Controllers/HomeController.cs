3a51278c53f50baa7209709a4f4fdb99d87df3b0
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-12-03 15:38:53 +0500

    Lecture 12
    Introduction to views.

---
+++ Lecture12/MVCViewsIntro/MVCViewsIntro/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCViewsIntro.Models;
+
+namespace MVCViewsIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            //return Json(new Country { ID = 1, Name = "Pakistan" });
+            //return View("Default");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Lecture12/MVCViewsIntro/MVCViewsIntro/Models/MVCViewsIntroContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCViewsIntro.Models
+{
+    public class MVCViewsIntroContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http ://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MVCViewsIntroContext() : base("name=MVCViewsIntroContext")
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+    }
+}
