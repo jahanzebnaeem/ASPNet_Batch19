f1df01b57a98d1b743bda2ded5081660fc56d296
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-12-04 21:14:01 +0500

    Lecture 14
    ASP Identity - Introduction

---
+++ Lecture14/ASPIdentity/ASPIdentity/Migrations/201912041605276_UpdatedAccountViewModel.cs
@@ -0,0 +1,16 @@
+namespace ASPIdentity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdatedAccountViewModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Lecture14/ASPIdentity/ASPIdentity/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Owin;
+using Owin;
+using ASPIdentity.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+[assembly: OwinStartupAttribute(typeof(ASPIdentity.Startup))]
+namespace ASPIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRolesandUsers();
+        }
+
+        private void CreateRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManger = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "Jahanzeb";
+                user.Email = "jahanzeb.naeem@hotmail.com";
+
+                string usrPWD = "J@Z201908";
+
+                var chkUsr = userManger.Create(user, usrPWD);
+
+                if (chkUsr.Succeeded)
+                {
+                    var result = userManger.AddToRole(user.Id, "Admin");
+                }
+
+            }
+
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
